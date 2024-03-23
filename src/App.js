322771b0ea786b0bfac854f943081a8b052cc566
Afinnity1 <omotolafinni089@gmail.com>
2024-03-23 21:57:38 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -2,17 +2,8 @@ import React, { useState } from 'react';
 import { useEffect } from 'react';
 import './App.css';
 import LoginSignup from './containers/LoginSignup.js';
-import Location from './containers/Location';
-import Geomap from './containers/Geomap';
-import Review from './containers/Review';
-import Bottom from './containers/Bottom';
-import Navbar from './containers/Navbar';
-
-//import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-//import LocationTracker from './LocationTracker';
-//import ParticlesBg from 'particles-bg'
-/*import Table from './Table.js'*/
-// import {BrowserRouter,Routes, Route} from 'react-router-dom'
+import ZildoPage from './containers/ZildoPage';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 const App = () => {
 
@@ -26,26 +17,22 @@ const App = () => {
 
   return(
     <div className={`containerr ${theme}`}>
-      <Navbar theme={theme} setTheme={setTheme}/>
-      
-       {/* <Router>
+      {/*<ZildoPage theme={theme} setTheme={setTheme} /> */}
+      <Router>
         <Switch>
-          <Route path="/" exact component={LocationTracker} />
+          <Route path="/" exact component={ZildoPage} />
           <Route path="/login" component={LoginSignup} />
           <Route path="/signup" component={LoginSignup} />
         </Switch>
-      </Router> */}
-      {/*<BrowserRouter>
+      </Router>
+      <BrowserRouter>
         <Routes>
           <Route path='/' element={<LoginSignup/>} ></Route>
         </Routes> 
-        </BrowserRouter>*/}
-         {/*<ParticlesBg type="tadpole" bg={true} /> */}
-          {/*<LoginSignup/> */}
-          <Location />
-          <Geomap /> 
-          <Review />
-          <Bottom />
+      </BrowserRouter>
+      <ParticlesBg type="tadpole" bg={true} />
+      <LoginSignup/> 
+          
     </div>
   );
 }
